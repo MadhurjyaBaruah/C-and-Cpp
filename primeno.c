7b11ec65473985d26c17cb418e6c1fcbe808f918
Madhurjya Baruah <madhubaruah16@gmail.com>
2026-06-20 22:28:12 +0530

    minor changes

--- primeno.c
+++ primeno.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int n,1,i;
+	int n,1,i=1;
 	printf("Enter a number :");
 	scanf("%d", &n);
 	if (n>=1){
 		for(i=1;i<=n;i++){
-			print("%d",1);
+			print("%d",n);
 		}
 		printf("\n");
 }
